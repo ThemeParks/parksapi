d88507b77379d122f82dbc8c3bd35fc2b20c17c1
cubehouse <me@jamie.holdings>
2020-08-09 10:47:57 +0100

    [!] Match on boundary * Will now work for single-letter variables

--- scripts/esdoc/optionalChaining.cjs
+++ scripts/esdoc/optionalChaining.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   onHandleCode(event) {
     // remove any optional chaining use from the incoming doc
-    event.data.code = event.data.code.replace(/\w+\?\.\w/g, (substr) => {
+    event.data.code = event.data.code.replace(/\w+\?\.\b/g, (substr) => {
       return substr.replace(/\?/g, '');
     });
   },
