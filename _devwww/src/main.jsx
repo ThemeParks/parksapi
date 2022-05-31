89a5c96ad50e8dc2f141354b7036b2b8b9f1909c
Jamie Holding <me@jamie.holdings>
2022-05-31 20:48:42 +0100

    [+] debugview framework start

---
+++ _devwww/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <h1>parksapi debug view</h1>
+  </React.StrictMode>
+);
