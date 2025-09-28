0eefae3eadc028cb312671589979ece52cd4a063
Jamie Holding <me@jamie.holdings>
2025-09-28 07:35:42 +0100

    [+] Export schedules from destination object

--- src/destination.ts
+++ src/destination.ts
@@ -1,4 +1,4 @@
-import {LiveData, Entity} from "@themeparks/typelib";
+import {LiveData, Entity, EntitySchedule} from "@themeparks/typelib";
 
 // Base class for all destinations
 export abstract class Destination {
@@ -25,5 +25,15 @@ export abstract class Destination {
   async getLiveData(): Promise<LiveData[]> {
     throw new Error("getLiveData not implemented.");
   }
+
+  /**
+   * Get schedules for all entities in this destination
+   * @returns {EntitySchedule[]} List of schedules for entities
+   */
+  async getSchedules(): Promise<EntitySchedule[]> {
+    throw new Error("getSchedules not implemented.");
+  }
+
+  // TODO - http injector
 
 };
