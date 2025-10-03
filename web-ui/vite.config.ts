b41c0540353bf72dc5afc732afbcd854b9e8c051
Jamie Holding <me@jamie.holdings>
2025-10-03 06:26:13 +0000

    [+] Web-UI dev command allows iteration on frontend while running backend

--- web-ui/vite.config.ts
+++ web-ui/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8888',
         changeOrigin: true,
       },
     },
